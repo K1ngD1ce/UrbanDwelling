a445c719597eb0c0c748f122e2042852532749bc
MThor <118614844+K1ngD1ce@users.noreply.github.com>
2024-08-15 21:54:12 +0300

    Add

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 window.onload = () => {
     let input = document.querySelector('#input');
     input.oninput = function () {
-        let value = this.value.trim();
+        let value = this.value.trim().toLowerCase();
         let list = document.querySelectorAll('.catalog__item');
+        console.log(value);
+        
 
 
 
         if (value != '') {
             list.forEach(el => {
-                if (el.innerText.search(value) == -1) {
+                if (el.innerText.toLowerCase().search(value) == -1) {
                     el.classList.add('hide');
                 }
             });
